be6952fd0334b1d019406738abb357f386d461cf
tatuvesa <tatuve@metropolia.fi>
2024-01-18 13:54:03 +0200

    Update tunti1.py

--- tunti1.py
+++ tunti1.py
@@ -1,4 +1,20 @@
-print("Hei, maailma!")
+#eka = -9
+#toka = 12_456_123_180
+#kolmas = 4.973
+#neljäs = -4 + 2j
 
-print("Hei, maailma!")
+#print(eka)
+#print(toka)
+#print(kolmas)
+#print(neljäs)
+
+#print("Hei,\nmaailma!\n")
+
+fahrenheit_str = input('Anna lämpötila farenheit asteina: ')
+fahrenheit = float(fahrenheit_str)
+celsius = (fahrenheit-32)*5/9
+print("Lämpötila Celsius-asteina: " + str(celsius))
+
+#user = input('\nWhat do you want to say hello to?: ')
+#print("\nHello, " + user + "!\n")
 
