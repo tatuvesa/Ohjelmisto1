95b17facb8fd9c4d4b1ef7c1bbd1e5d1357a74a4
tatuvesa <tatuve@metropolia.fi>
2024-01-26 11:12:55 +0200

    add

---
+++ tuntityöt/tunti5.py
@@ -0,0 +1,9 @@
+import math
+def tervehdi():
+    print(f"Moi!")
+    print(f"{math.pi}")
+    return
+
+print(f"Tämä tulostus tulee pääohjelmasta\n")
+tervehdi()
+print(f"\nTämä tulostus tulee tervehtimisen jälkeen.")
