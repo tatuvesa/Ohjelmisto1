68e48fa2b73943848ce3b06706f18ac31574ec81
tatuvesa <tatuve@metropolia.fi>
2024-02-05 10:16:01 +0200

    add

---
+++ tuntityöt/tunti6sql.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+def haekentta(nimi):
+    sql = "select name, ident from airport where name like '%" + nimi + "%'"
+    kursori = mydb.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            print(f"Lentokentän kokonimi on {rivi[0]} ja sen ICAO on {rivi[1]}")
+    return
+
+mydb = mysql.connector.connect(
+       host='localhost',
+       port='3306',
+       database='flight_game',
+       user='root',
+       password='5alattu',
+       autocommit=True
+       )
+kentanNimi = input("Anna kentän nimi: ")
+haekentta(kentanNimi)
