ab0b0a5881d8ae2c0576fa1f14b210510fae2f1b
tatuvesa <tatuve@metropolia.fi>
2024-02-05 11:08:11 +0200

    add

--- tuntityöt/tunti6sql.py
+++ tuntityöt/tunti6sql.py
@@ -2,7 +2,6 @@ import mysql.connector
 
 def haekentta(nimi):
     sql = "select name, ident from airport where name like '%" + nimi + "%';"
-    print(sql)
     kursori = mydb.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
@@ -10,6 +9,11 @@ def haekentta(nimi):
         for rivi in tulos:
             print(f"Lentokentän kokonimi on {rivi[0]} ja sen ICAO on {rivi[1]}")
     return
+def hae(sql):
+    kursori = mydb.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
 
 mydb = mysql.connector.connect(
        host='localhost',
@@ -19,5 +23,25 @@ mydb = mysql.connector.connect(
        password='5alattu',
        autocommit=True
        )
-kentanNimi = input("Anna kentän nimi: ")
-haekentta(kentanNimi)
+while True:
+    print("1: kentän suppeat tiedot.")
+    print("2: kentän laajat tiedot.")
+    print("Jätä kenttä tyhjäksi poistuaksesi.")
+
+    kentanNimi = input("Anna kentän nimi: ")
+    valinta = input("Valitse 1 tai 2: ")
+
+    if valinta == "1":
+        haekentta(kentanNimi)
+
+    elif valinta == "2":
+        sql = "select ident, airport.name, country.name from airport, country where airport.iso_country = country.iso_country and airport.name like '%" + kentanNimi + "%';"
+        tulos = hae(sql)
+        for rivi in tulos:
+            print(rivi[0], rivi[1], rivi[2])
+
+    elif valinta != "1" and valinta != "2" or kentanNimi == "":
+        break
+
+    else:
+        print("Virheellinen syöte.")
